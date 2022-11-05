7a8c30b42663153fbb8c7a7e273497702aeef674
InfinteEndeavours <ethan.pitt@protonmail.com>
2022-11-05 22:08:19 +0000

    Add examples of arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers(a, b){
+    // Code block 
+    return a + b;
+}
+let sum1 = addTwoNumbers(1, 1);
+console.log(sum1);
 // Arrow Function With Parameters
-
+const addTwoNum = (a, b) => {
+    // Code block 
+    return a + b;
+}
+let sum2 = addTwoNum(4, 1);
+console.log(sum2);
 // Single Line Arrow Function With Parameters
+const twoNumbers = (a, b) => a + b;
 
+let sum3 = twoNumbers(4, 4);
+console.log(sum3);
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!');
 
+const sayHello =()=>console.log('Hello!');
+sayHello();
 // Returning Multiple Lines
+const returnMulti = () => (
+    `<p>
+        Test
+    </p>`
+);
+
+console.log(returnMulti());
